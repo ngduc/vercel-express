faaa787d4dbb3513c8306e043282cb4c6cb67844
Duc Ng <ducjava@gmail.com>
2021-02-23 06:49:09 +0000

    chore: removed uuid lib

--- app/index.js
+++ app/index.js
@@ -1,16 +1,16 @@
 const app = require('express')();
-const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const randomId = `${Math.random()}`.slice(2);
+  const path = `/api/item/${randomId}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+app.get('/api/item/:itemId', (req, res) => {
+  const { itemId } = req.params;
+  res.end(`Item: ${itemId}`);
 });
 
 module.exports = app;
